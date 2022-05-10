150ddeee8012fcfff6198675fc2df7ba48524a54
unknown <stamandtyler@gmail.com>
2022-05-10 18:59:16 -0400

    Walk Animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,9 +9,11 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] float moveForce;
     [SerializeField] float topSpeed;
     new Rigidbody2D rigidbody;
+    Animator animator;
 
     void Start() {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         rigidbody.gravityScale = 0;
     }
 
@@ -20,6 +22,10 @@ public class PlayerController : MonoBehaviour {
         //Currently no smoothing is applied, change later if needed
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("X", horizontal);
+        animator.SetFloat("Y", vertical);
+
 
         //normalized to prevent moving quicker diagonally
         Vector2 inputMovement = new Vector2(horizontal, vertical).normalized;
